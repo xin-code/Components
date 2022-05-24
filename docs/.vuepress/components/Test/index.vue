6b1ff7d9cb79acce7807351f3096e70b6760c547
zxg <975203723@qq.com>
2022-05-24 17:06:41 +0800

    fix: :memo: 测试用例

---
+++ docs/.vuepress/components/Test/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>测试用例</span>
+
+    <div v-for="i in 10">{{i}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+    }
+  },
+  methods:{
+  }
+
+}
+</script>
+
+<style>
+</style>
